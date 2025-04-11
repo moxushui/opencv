bc0123dbcf6927b2c84fce89e4debeda055d0302
moxushui <zlz171552@gmail.com>
2025-04-11 08:37:25 +0800

    size.py

--- size.py
+++ size.py
@@ -1,11 +1,13 @@
+import shutil
+
 from PIL import Image
 import os
 def read_size(label_file_name,images_dir):
     for images_file_name in os.listdir(images_dir):
         if os.path.splitext(label_file_name)[0] == os.path.splitext(images_file_name)[0]:
              with Image.open(os.path.join(images_dir,images_file_name)) as img:
-                 width,height=img.size
-                 return width,height
+                 return img.size
+    raise FileNotFoundError(f"未找到与标签文件 {label_file_name} 对应的图片")
 
 def yolo_read(label_file_name,labels_dir):
     yolo_size_lsts =[]
@@ -32,22 +34,31 @@ def box_size(width,height,yolo_width, yolo_height):
         return '大目标'
 
 
+def move(a):
+    output_min_dir = r'C:\Users\Administrator\PycharmProjects\label\output_min_dir'
+    os.makedirs(output_min_dir, exist_ok=True)
+    if a == '极小目标':
+        shutil.move(os.path.join(labels_dir,label_file_name),os.path.join(output_min_dir,f'move_{label_file_name}'))
+        shutil.move(os.path.join(images_dir, f'{os.path.splitext(label_file_name)[0]}.jpg'), os.path.join(output_min_dir, f'move_{os.path.splitext(label_file_name)[0]}.jpg'))
+
 
 
 if __name__=='__main__':
-    images_dir = r'D:\python\PythonProject\output_dir\images'
-    labels_dir = r'D:\python\PythonProject\output_dir\labels'
-    output_dir = r'D:\python\PythonProject\box_size_dir'
+    images_dir = r'C:\Users\Administrator\PycharmProjects\label\label_images'
+    labels_dir = r'C:\Users\Administrator\PycharmProjects\label\yolo'
+    output_dir = r'C:\Users\Administrator\PycharmProjects\label\box_size_dir'
 
     os.makedirs(output_dir, exist_ok=True)
     for label_file_name in os.listdir(labels_dir):
-        width,height =read_size(label_file_name,images_dir)
+        if label_file_name == 'classes.txt':
+            continue
+        width,height = read_size(label_file_name,images_dir)
         yolo_size_lsts = yolo_read(label_file_name,labels_dir)
         for yolo_size_lst in yolo_size_lsts:
             yolo_width, yolo_height = yolo_size_lst
-            os.makedirs('box_size_dir',exist_ok=True)
+            a = box_size(width, height, yolo_width, yolo_height)
             with open(os.path.join(output_dir,f'{label_file_name}_box_size'),'w',encoding='utf-8') as f :
-                f.write(label_file_name,box_size(width, height, yolo_width, yolo_height))
+                f.write(f'{label_file_name},{a}')
 
 
 
